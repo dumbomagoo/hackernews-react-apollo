f0107e90e6c909a6fde27c28d439a2e2b1dc158b
Sarah Young <sarahyoung@libp45p-23560wp.lm.lmig.com>
2017-11-03 17:13:09 -0400

    Added stubs for Link components and refactored Hello component

---
+++ src/components/Hello.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { graphql, gql } from 'react-apollo';
+
+const Hello = props => {
+  const {
+    helloQuery: {
+      loading = false,
+      error = '',
+      hello = ''
+    }
+  } = props;
+  
+  return (
+    <div>
+    {
+      loading ? <div>Loading...</div> :
+      error ? <div>{error.message}<p>{error.stack}</p></div> :
+      <h1>{hello}</h1>
+    }
+    </div>
+  );
+};
+
+Hello.defaultProps = {
+  helloQuery: {}
+};
+
+const HELLO_QUERY = gql`
+  query HelloQuery {
+    hello 
+  }
+`;
+
+export default graphql(HELLO_QUERY, { name: 'helloQuery' })(Hello);
---
+++ src/components/LinkList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import Link from './Link';
+
+const LinkList = props => (
+  <div>
+
+  </div>
+);
+
+export default LinkList;
