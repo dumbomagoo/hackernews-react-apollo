439a5b4577086ed397ace61e7c2fdc2c871cc2bd
Sarah Young <sarahyoung@libp45p-23560wp.lm.lmig.com>
2017-11-06 14:27:14 -0500

    Added some routes and mutation for creating link

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
 
 import LinkList from './LinkList';
-import Hello from './Hello';
+import CreateLink from './CreateLink';
+import Header from './Header';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Hello />
-        <LinkList />
+      <div className={'center w85'}>
+        <Header />
+        <div className={'ph3 pv1 background-gray'}>
+          <Switch>
+            <Route exact path={'/'} component={LinkList} />
+            <Route exact path={'/create'} component={CreateLink} />
+          </Switch>
+        </div>
       </div>
     );
   }
